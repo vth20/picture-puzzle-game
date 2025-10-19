9698b3349d8248607250b088afa80d5ad46e47a3
vth20 <vanthanhhuynhtk@gmail.com>
2025-10-20 00:05:50 +0700

    [update] all image

--- constants.ts
+++ constants.ts
@@ -6,6 +6,25 @@ import img5 from "./assets/images/5_a_Quang.png";
 import img6 from "./assets/images/6_c_Le.png";
 import img7 from "./assets/images/7_a_Nhat.png";
 import img8 from "./assets/images/8_a_Thanh.png";
+import img9 from "./assets/images/9_a_Quan.png";
+import img10 from "./assets/images/10_c_My.png";
+import img11 from "./assets/images/11_a_Hoa.png";
+import img12 from "./assets/images/12_a_Quoc.png";
+// import img13 from "./assets/images/13_a_Thuan.png";
+// import img14 from "./assets/images/14_c_Giang.png";
+import img15 from "./assets/images/15_c_Phuong.png";
+import img16 from "./assets/images/16_c_ngan.png";
+import img17 from "./assets/images/17_c_Tram.png";
+import img18 from "./assets/images/18_c_trang.png";
+// import img19 from "./assets/images/19_c_van.png";
+import img20 from "./assets/images/20_c_vy.png";
+import img21 from "./assets/images/21_c_dieu.png";
+import img22 from "./assets/images/22_a_luong.png";
+// import img23 from "./assets/images/23_c_tram.png";
+import img24 from "./assets/images/24_a_toan.png";
+import img25 from "./assets/images/25_a_dat.png";
+import img26 from "./assets/images/26_a_luan.png";
+import img27 from "./assets/images/27_c_tram.png";
 
 import o0 from "./assets/original/0_a_Dat.png";
 import o1 from "./assets/original/1_a_Duc.png";
@@ -15,6 +34,25 @@ import o5 from "./assets/original/5_a_Quang.png";
 import o6 from "./assets/original/6_c_Le.png";
 import o7 from "./assets/original/7_a_Nhat.png";
 import o8 from "./assets/original/8_a_Thanh.png";
+import o9 from "./assets/original/9_a_Quan.png";
+import o10 from "./assets/original/10_c_My.jpg";
+import o11 from "./assets/original/11_a_Hoa.png";
+import o12 from "./assets/original/12_a_Quoc.jpg";
+// import o13 from "./assets/original/13_a_Thuan.jpg";
+// import o14 from "./assets/original/14_c_Giang.png";
+import o15 from "./assets/original/15_c_Phuong.jpg";
+import o16 from "./assets/original/16_c_ngan.jpg";
+import o17 from "./assets/original/17_c_Tram.jpg";
+import o18 from "./assets/original/18_c_trang.jpg";
+// import o19 from "./assets/original/19_c_van.png";
+import o20 from "./assets/original/20_c_vy.png";
+import o21 from "./assets/original/21_c_dieu.jpg";
+import o22 from "./assets/original/22_a_luong.png";
+// import o23 from "./assets/original/23_c_tram.png";
+import o24 from "./assets/original/24_a_toan.png";
+import o25 from "./assets/original/25_a_dat.png";
+import o26 from "./assets/original/26_a_luan.png";
+import o27 from "./assets/original/27_c_tram.jpg";
 export const GRID_OPTIONS: number[] = [2, 3, 4, 6];
 
 // Using placeholder images. Replace these with your actual company photos.
@@ -55,5 +93,73 @@ export const IMAGE_URLS: TData[] = [
   {
     image: img8,
     original: o8
+  },
+  {
+    image: img9,
+    original: o9
+  },
+  {
+    image: img10,
+    original: o10
+  },
+  {
+    image: img11,
+    original: o11
+  },
+  {
+    image: img12,
+    original: o12
+  },
+  {
+    image: img15,
+    original: o15
+  },
+  {
+    image: img16,
+    original: o16
+  },
+  {
+    image: img17,
+    original: o17
+  },
+  {
+    image: img18,
+    original: o18
+  },
+  // {
+  //   image: img19,
+  //   original: o19
+  // },
+  {
+    image: img20,
+    original: o20
+  },
+  {
+    image: img21,
+    original: o21
+  },
+  {
+    image: img22,
+    original: o22
+  },
+  // {
+  //   image: img23,
+  //   original: o23
+  // },
+  {
+    image: img24,
+    original: o24
+  },
+  {
+    image: img25,
+    original: o25
+  },
+  {
+    image: img26,
+    original: o26
+  },
+  {
+    image: img27,
+    original: o27
   },
 ];
